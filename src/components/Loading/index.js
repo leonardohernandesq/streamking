3fe524dd51134ce7d6c352eb36e3892090801169
Leonardo Hernandes <leonardo_hernandes@outlook.com.br>
2024-11-15 15:09:19 +0000

    Alteracoes

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import './loading.css';
+
+function Loading() {
+    return (
+        <div className='loading'>
+            <span></span>
+            <h2>Carregando Filmes...</h2>
+        </div>
+    );
+}
+
+export default Loading;
